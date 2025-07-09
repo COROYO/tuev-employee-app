da517c6df73f9e6529b5370cf37d3b52ded6df0a
alexgrot <alexgrot@gmx.de>
2025-07-09 21:27:18 +0200

    login/register page layout

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -8,10 +8,9 @@
 	let { form }: { form: ActionData } = $props();
 </script>
 
-<h1>Login/Register</h1>
 <form method="post" class="flex flex-col gap-2" action="?/login" use:enhance>
 	<div class="flex flex-col gap-2 max-w-md">
-		<div class="flex items-center space-x-2">
+		<div class="flex-1">
 			<Label for="username">
 				Username
 			</Label>
@@ -21,7 +20,7 @@
 				class="mt-1 rounded-md border border-gray-300 bg-white px-3 py-2 shadow-sm focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none"
 			/>
 		</div>
-		<div class="flex items-center space-x-2">
+		<div class="flex-1">
 			<Label for="password">
 				Password
 			</Label>
